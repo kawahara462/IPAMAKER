565bb136b8a2aa0ec55c0d0a62d1411f5ca5da2a
kawahara462 <guriguramonya@gmail.com>
2026-03-16 21:15:01 +0900

    Update buildManager.js

--- buildManager.js
+++ buildManager.js
@@ -1,20 +1,19 @@
-let builds={}
+let builds = {}
 
 export function checkLimit(user){
 
- let now=Date.now()
+ let now = Date.now()
 
- if(!builds[user]) builds[user]=[]
+ if(!builds[user]) builds[user] = []
 
- builds[user]=builds[user].filter(t=>now-t<86400000)
+ builds[user] = builds[user].filter(t => now - t < 86400000)
 
- let lastHour=builds[user].filter(t=>now-t<3600000)
+ let lastHour = builds[user].filter(t => now - t < 3600000)
 
- if(lastHour.length>=1) return false
- if(builds[user].length>=3) return false
+ if(lastHour.length >= 1) return false
+ if(builds[user].length >= 3) return false
 
  builds[user].push(now)
 
  return true
-
 }
